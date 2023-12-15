5023a980946bb8e9a5e13d754fdb23aef1c62b86
Abeeb <abeebdon@gmail.com>
2023-12-15 10:24:25 +0100

    Made it responsive successfully

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const share = document.getElementById('share')
 const shared = document.getElementById('shared')
 const clicked = document.getElementById('clicked')
 const footer = document.getElementById('footer')
+const icon = document.getElementById('icon')
+const icon1 = document.getElementById('icon1')
 const handleClick = () => {
   clicked.style.display = 'flex'
-  footer.style.display = 'none'
+  icon1.style.display = 'block'
+  icon.style.display = 'none'
 }
 const handleClick1 = () => {
   clicked.style.display = 'none'
-  footer.style.display = 'flex'
+  icon1.style.display = 'none'
+  icon.style.display = 'block'
 }
 
 share.addEventListener('click', handleClick)
-shared.addEventListener('click', handleClick1)
+icon1.addEventListener('click', handleClick1)
